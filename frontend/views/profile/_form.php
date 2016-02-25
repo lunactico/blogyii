c3f8e54d369688a7bc3654129dd002dccfb4e1d6
System Administrator <root@MacBook-Pro-de-lunactico.local>
2016-02-25 08:50:41 -0500

    correccion de errors

--- frontend/views/profile/_form.php
+++ frontend/views/profile/_form.php
@@ -2,29 +2,36 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
-use yii\jui\DatePicker;
 
-/* @var $this yii\web\View */
-/* @var $model frontend\models\Profile */
-/* @var $form yii\widgets\ActiveForm */
+/**
+ * @var yii\web\View $this
+ * @var app\models\Profile $model
+ * @var yii\widgets\ActiveForm $form
+ */
 ?>
 
 <div class="profile-form">
 
+    <?php $form = ActiveForm::begin(); ?>
+    
     <?= $form->field($model, 'first_name')->textInput(['maxlength' => 45]) ?>
-	
-	<?= $form->field($model, 'last_name')->textInput(['maxlength' => 45]) ?>
-<br/>
-	
-	<?php echo $form->field($model,'birthdate')->widget(DatePicker::className(),['clientOptions' => ['dateFormat' => 'yy-mm-dd']]); ?>
-<br/>
-	
-	<?= $form->field($model, 'gender_id')->dropDownList($model->genderList,['prompt' => 'Please Choose One' ]);?>
 
+    <?= $form->field($model, 'last_name')->textInput(['maxlength' => 45]) ?>
+<br>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
+    <?= $form->field($model, 'birthdate')->textInput() ?>
+ 	* please use YYYY-MM-DD format
+ 
+<br>       
+
+<?= $form->field($model, 'gender_id')->dropDownList($model->genderList, ['prompt' => 'Please Choose One' ]);?>
+        
+<div class="form-group">
+
+<?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', 
+['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        
+        </div>
 
     <?php ActiveForm::end(); ?>
 
